6482f41dd513cc30c0def11e31c64f9f9cc4f2a6
Sandrolek <sandrolek06@gmail.com>
2026-05-14 12:37:12 +0300

    updated benchmarks

--- students/K3339/Poliakov_Aleksandr/Lr2/task1/benchmark.py
+++ students/K3339/Poliakov_Aleksandr/Lr2/task1/benchmark.py
@@ -40,7 +40,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main() -> None:
+def main():
     args = parse_args()
     root = Path(__file__).resolve().parent
 
--- students/K3339/Poliakov_Aleksandr/Lr2/task2/benchmark.py
+++ students/K3339/Poliakov_Aleksandr/Lr2/task2/benchmark.py
@@ -34,7 +34,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main() -> None:
+def main():
     args = parse_args()
     root = Path(__file__).resolve().parent
 
